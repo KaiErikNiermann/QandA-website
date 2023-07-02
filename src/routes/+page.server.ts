1e565af2235e49c93bf3fa31f4d41898307ba811
Kai Erik Niermann <kai.niermann@gmail.com>
2023-07-02 22:11:24 +0200

    added a bunch more stuff

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,12 +1,40 @@
 import { questions } from "../db/questions";
+import { client } from "../db/mongo";
 
-/** @type {import('./$types').PageServerLoad} */
 export async function load() {
-    const data = await questions.find({}, {limit: 50, projection: {
-        _id: 0,
-    }}).toArray();
+    const data = await questions
+        .find(
+            {},
+            {
+                limit: 100,
+                projection: {
+                    _id: 0,
+                },
+            }
+        )
+        .toArray();
 
     return {
-        questions: data
+        questions: data,
     };
 }
+
+export const actions: import("./$types").Actions = {
+    add_answer: async ({ request }) => {
+        const data = await request.formData();
+        const answer = data.get("answer");
+
+        console.log("Adding answer to question: " + data.get("question"));
+        client
+            .db("main_db")
+            .collection("QandA_collection")
+            .updateOne({
+                question: data.get("question"),
+            },
+            {
+                $set: {
+                    answer: answer,
+                }
+            });
+    },
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
     import type { PageData } from "./$types";
-
     type db_listing = {
         question: string;
         guild_id: string;
-        status: number;
+        status: string;
     };
+
     let questions: db_listing[] = [];
     let curr_guild_id: string;
 
@@ -23,7 +23,7 @@
 
 <h1>FaQ page</h1>
 
-<input bind:value={curr_guild_id}/>
+<input bind:value={curr_guild_id} />
 <ul>
     {#each questions as question}
         {#if question.guild_id == curr_guild_id}
@@ -31,6 +31,14 @@
                 <h3>
                     {question.question}
                 </h3>
+                <form method="POST" action="?/add_answer">
+                    <label>
+                        Answer:
+                        <input name="answer">
+                        <input type="hidden" name="question" value={question.question}>
+                    </label>
+                    <button type="submit">Submit</button>
+                </form>
             </li>
         {/if}
     {/each}
