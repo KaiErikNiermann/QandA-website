802f0c4ff6ea97fd545483004f390b8be4456cce
Kai Erik Niermann <kai.niermann@gmail.com>
2023-07-12 07:11:29 +0200

    basic properly working sync

---
+++ src/routes/api/questions/+server.ts
@@ -0,0 +1,25 @@
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { client } from '$lib/db';
+
+export const GET = (async ({ url, locals }) => {
+    const data = await client.db("main_db").collection("QandA_collection").find(
+        {},
+        {
+            limit: 1000,
+            projection: {
+                _id: 0,
+            },
+        }
+    ).toArray();
+
+    if (locals.wss) {
+
+		locals.wss.clients.forEach((connected_client: WebSocket) => {
+			if (connected_client.readyState === 1) {
+				connected_client.send(`Hello from the GET handler at ${new Date().toLocaleString()}`);
+			}
+		});
+	}
+	return json(data);
+}) satisfies RequestHandler;
