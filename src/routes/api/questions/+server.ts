8934f2da5f54c4a19c143d10f893a7cf9a8020b2
KaiErikNiermann <kai.niermann@gmail.com>
2023-08-25 02:08:55 +0200

    added deps for markdown

--- src/routes/api/questions/+server.ts
+++ src/routes/api/questions/+server.ts
@@ -2,7 +2,7 @@ import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import { client } from '$lib/db';
 
-export const GET = (async ({ url, locals }) => {
+export const GET = (async ({ locals }) => {
     const data = await client.db("main_db").collection("QandA_collection").find(
         {},
         {
@@ -14,12 +14,12 @@ export const GET = (async ({ url, locals }) => {
     ).toArray();
 
     if (locals.wss) {
-
 		locals.wss.clients.forEach((connected_client: WebSocket) => {
 			if (connected_client.readyState === 1) {
 				connected_client.send(`Hello from the GET handler at ${new Date().toLocaleString()}`);
 			}
 		});
 	}
+    
 	return json(data);
 }) satisfies RequestHandler;
--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,44 @@
-import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-auto";
+import remarkUnwrapImages from 'remark-unwrap-images'
+import remarkToc from 'remark-toc'
+import remarkMath from "remark-math";
+import rehypeSlug from 'rehype-slug'
+import rehypeKatexSvelte from "rehype-katex-svelte";
+import { vitePreprocess } from "@sveltejs/kit/vite";
+import { mdsvex } from 'mdsvex';
+
+/** @type {import('mdsvex').MdsvexOptions} */
+const mdsvexOptions = {
+	extensions: ['.md'],
+  remarkPlugins: [
+    remarkMath,
+    remarkUnwrapImages, 
+    [remarkToc, { tight: true }],
+  ],
+  // Render katex components inside @html blocks, aka {@html "<katex output html>"}
+  rehypePlugins: [
+    rehypeKatexSvelte,
+    rehypeSlug    
+  ],
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [vitePreprocess({})],
+  extensions: [".svelte", ...mdsvexOptions.extensions],
+  preprocess: [
+    vitePreprocess(),
+    mdsvex(mdsvexOptions),
+    preprocess({
+      scss: {
+        prependData: '@use "src/variables.scss" as *;',
+      },
+    })
+  ],
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter(),
+  },
 };
 
 export default config;
